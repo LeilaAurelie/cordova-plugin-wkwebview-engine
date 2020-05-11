9c7342a2df68c12f631933f362af1ccc7cfcb394
Leila Elkhalidy <lelkhalidy@atb.com>
2020-05-11 11:38:59 -0600

    set UIScrollView property to fix bottom padding
    set UIScrollViewContentInsetAdjustmentNever property to fix bottom padding on viewport.

--- src/ios/CDVWKWebViewEngine.m
+++ src/ios/CDVWKWebViewEngine.m
@@ -97,6 +97,11 @@
 
     // re-create WKWebView, since we need to update configuration
     WKWebView* wkWebView = [[WKWebView alloc] initWithFrame:self.engineWebView.frame configuration:configuration];
+  
+    if (@available(iOS 11.0, *)) {
+      [wkWebView.scrollView setContentInsetAdjustmentBehavior:UIScrollViewContentInsetAdjustmentNever];
+    }
+    
     wkWebView.UIDelegate = self.uiDelegate;
     self.engineWebView = wkWebView;
 
